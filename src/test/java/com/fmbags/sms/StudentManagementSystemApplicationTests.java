4573ec00852e2e062d3e480261afa6e7d5a90951
fmbags <baguion445@gmail.com>
2022-03-14 19:17:57 +0800

    Spring Boot Starter Project

---
+++ src/test/java/com/fmbags/sms/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmbags.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
